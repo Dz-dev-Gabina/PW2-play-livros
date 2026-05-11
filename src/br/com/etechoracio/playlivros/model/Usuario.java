e8507d2e9883f3b58cdf59d2a7ade9a8f6a683a3
Dz-dev-Gabina <davihzjapao@gmail.com>
2026-05-11 08:12:24 -0300

    Criação de construtor em Usuario

--- src/br/com/etechoracio/playlivros/model/Usuario.java
+++ src/br/com/etechoracio/playlivros/model/Usuario.java
@@ -3,4 +3,9 @@ package br.com.etechoracio.playlivros.model;
 public class Usuario {
     public String foto;
     public String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
 }
--- src/br/com/etechoracio/playlivros/test/TestaAvaliacao.java
+++ src/br/com/etechoracio/playlivros/test/TestaAvaliacao.java
@@ -7,27 +7,26 @@ import java.time.LocalDateTime;
 
 public class TestaAvaliacao {
     static void main() {
-        Usuario horacio =  new Usuario();
-        horacio.nome = "Horacio Augusto da Silveira";
-
-        Usuario renata = new Usuario();
-        renata.nome = "Renata Maciel";
 
         Avaliacao primeiraAvaliacao = new Avaliacao();
-        primeiraAvaliacao.usuario = horacio;
-        primeiraAvaliacao.adicionar(5);
+
+        primeiraAvaliacao.usuario = new Usuario("Horacio Augusto da Silveira");
+        primeiraAvaliacao.adicionar(5,"Bom","Gostei bastante do livro, parabens!!!");
         primeiraAvaliacao.data = LocalDateTime.now();
         primeiraAvaliacao.aprovado = true;
 
         primeiraAvaliacao.exibirDados();
+        System.out.print("\n");
+        //----------------------------------------------
 
         Avaliacao segundoAvaliacao = new Avaliacao();
-        segundoAvaliacao.usuario = renata;
+        segundoAvaliacao.usuario = new Usuario("Renata Maciel");
         segundoAvaliacao.adicionar(5,"Bom","Para mim que tenho filho maior que 5 anos, não achei interessante.");
         segundoAvaliacao.data = LocalDateTime.now();
         segundoAvaliacao.aprovado = true;
 
         segundoAvaliacao.exibirDados();
-
+        System.out.print("\n");
+        //-----------------------------------------------
     }
 }
